c08e596b5094042f98be10bbed0513416835f323
Alejo Carobolante <alejocarobolante@gmail.com>
2025-10-11 18:32:23 -0300

    Template completed

--- api/route/entity1_route.go
+++ api/route/entity1_route.go
@@ -2,22 +2,13 @@ package route
 
 import (
 	"gorm-template/bootstrap"
-	"gorm-template/pkg/usecase"
 	"time"
 
-	"gorm-template/api/controller"
+	//	"gorm-template/api/controller"
 
 	"github.com/gin-gonic/gin"
 )
 
 func NewEntityRouter(env *bootstrap.Env, timeout time.Duration, group *gin.RouterGroup) {
-	ec := &controller.Entity1Controller{
-		Entity1Repository: &usecase.Entity1UseCase{},
-	}
-	entity1Routes := group.Group("/entity1")
-	entity1Routes.POST("", ec.Create)
-	entity1Routes.GET("", ec.Fetch)
-	entity1Routes.GET(":id", ec.FetchById)
-	entity1Routes.PUT("", ec.Update)
-	entity1Routes.DELETE(":id", ec.Delete)
+	//ec := &controller.Entity1{}
 }
